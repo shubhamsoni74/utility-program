7c27cb60eb13ba97a5571db9944ea065ef0d3d45
shubhamsoni6050 <40332828+shubhamsoni6050@users.noreply.github.com>
2018-08-07 15:05:05 +0530

    initial commit

---
+++ NewProject7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;   
+using System.Linq;          
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewProject7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Name; int Age; float Marks;
+
+            Console.Write(" Your Name is:");
+            Name = Console.ReadLine();
+            Console.WriteLine("---");
+            Console.Write(" Your Age is:");
+            Age = int.Parse(Console.ReadLine());
+            Console.WriteLine("---");
+            Console.Write(" Your Marks Are:");
+            Marks = float.Parse(Console.ReadLine());
+            Console.WriteLine("----");
+            Console.WriteLine("First Type");
+            Console.WriteLine("Your Name is: {0} Your Age is: {1} And your Marks are: {2 }", Name, Age, Marks);
+            Console.WriteLine("----");
+            Console.WriteLine("Second Type");
+            Console.WriteLine("Your Name is:" + Name + "Your Age is:" + Age + "Your Marks are:" + Marks);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();  
+        }
+    }
+}
